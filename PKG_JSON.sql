5243bbe0654fa2815b4d7f49cfab04ed14f49d39
ilya-taytslin <62900642+ilya-taytslin@users.noreply.github.com>
2023-09-11 10:03:40 -0400

    Add files via upload

--- PKG_JSON.sql
+++ PKG_JSON.sql
@@ -61,6 +61,7 @@ END PKG_JSON;
 create or replace PACKAGE BODY PKG_JSON AS
 
     isi_CurrentDebugLevel SMALLINT := OFF_DEBUG_LEVEL;
+    ITERATION_LENGTH CONSTANT NUMBER := 4000;
     AREA_CODE CONSTANT VARCHAR2(9) := 'AREA_CODE';
     DEALER_PERMIT CONSTANT VARCHAR2(20) := 'DEALER_PERMIT_NUMBER';
     DEALER_NAME CONSTANT VARCHAR2(11) := 'DEALER_NAME';
@@ -312,8 +313,6 @@ END ;
         v_result_length NUMBER := 0;
         v_outputed_length NUMBER := 1;   -- Because first position of a string in PL/SQL is 1, not 0
         
-        ITERATION_LENGTH CONSTANT NUMBER := 4000;
-        
         GEAR_CODE CONSTANT VARCHAR2(4) := 'CODE';
         GEAR_NAME CONSTANT VARCHAR2(4) := 'NAME';
         MESH_MINIMUM CONSTANT VARCHAR2(12) := 'MESH_MINIMUM';
@@ -525,8 +524,6 @@ procedure dealer_json (v_input IN CLOB, v_result OUT clob )
         v_timestamp NUMBER := 0;
         v_filecode NUMBER := 0;
         
-        ITERATION_LENGTH CONSTANT NUMBER := 4000;
-        
         dealer_array  JSON_ARRAY_T := json_array_t();
         empty_array  JSON_ARRAY_T := json_array_t();
   begin
@@ -568,20 +565,7 @@ procedure dealer_json (v_input IN CLOB, v_result OUT clob )
                 SELECT 99998 as dealer_permit_number,'Home Consumption' as DEALER_NAME FROM dual)  )
         select d.dealer_permit_number, d.DEALER_NAME
         from dealer_data d) loop
-        /*
-            dealer_object.put(DEALER_PERMIT, d_data.DEALER_PERMIT_NUMBER);
-            dealer_object.put(DEALER_NAME, d_data.DEALER_NAME);
 
-             IF ( isi_CurrentDebugLevel >= VERBOSE_DEBUG_LEVEL ) THEN
-                DBMS_OUTPUT.PUT_LINE('Dealer record:' || dealer_object.to_clob());
-             END IF;
-             
-            --Add object to array
-            dealer_array.append(dealer_object);
-            --START cleaning of JSON object. This is needed per key
-            dealer_object.remove(DEALER_PERMIT);
-            dealer_object.remove(DEALER_NAME);
-        */
             dealer_array_append (dealer_array, d_data.DEALER_PERMIT_NUMBER, d_data.DEALER_NAME);
         end loop;
     ELSE
@@ -663,8 +647,6 @@ procedure dealer_json (v_input IN CLOB, v_result OUT clob )
         v_maxdate date;
         v_result_length NUMBER := 0;
         v_outputed_length NUMBER := 1;   -- Because first position of a string in PL/SQL is 1, not 0
-        
-        ITERATION_LENGTH CONSTANT NUMBER := 4000;
         
         DEALER_PERMIT_WITH_NAME CONSTANT VARCHAR2(23) := 'DEALER_PERMIT_WITH_NAME';
       
@@ -749,8 +731,6 @@ procedure dealer_json (v_input IN CLOB, v_result OUT clob )
         v_maxdate date;
         v_result_length NUMBER := 0;
         v_outputed_length NUMBER := 1;   -- Because first position of a string in PL/SQL is 1, not 0
-        
-        ITERATION_LENGTH CONSTANT NUMBER := 4000;
         
         VESSEL_PERMIT CONSTANT VARCHAR2(20) := 'VESSEL_PERMIT_NUMBER';
         VESSEL_NAME CONSTANT VARCHAR2(11) := 'VESSEL_NAME';
@@ -863,8 +843,6 @@ end;
         v_timestamp NUMBER := 0;
         v_filecode NUMBER := 0;
         
-        ITERATION_LENGTH CONSTANT NUMBER := 4000;
-      
         operator_object  JSON_OBJECT_T := json_object_t();
         operator_array  JSON_ARRAY_T := json_array_t();
         empty_array  JSON_ARRAY_T := json_array_t();
@@ -977,8 +955,6 @@ procedure permit_json (v_input IN CLOB, v_result OUT clob )
         v_outputed_length NUMBER := 1;   -- Because first position of a string in PL/SQL is 1, not 0
         v_timestamp NUMBER := 0;
         v_filecode NUMBER := 0;
-        
-        ITERATION_LENGTH CONSTANT NUMBER := 4000;
         
         PNUM CONSTANT VARCHAR2(4) := 'PNUM';
         VES_NAME CONSTANT VARCHAR2(8) := 'VES_NAME';
@@ -1177,23 +1153,21 @@ procedure permit_json (v_input IN CLOB, v_result OUT clob )
         v_loop  NUMBER;
         v_maxdate date;
         v_exclude_rpt_type NUMBER := 0;
-        x_exist NUMBER := 0;    -- Flag indicating whether a species is excluded from some report types
-        
-        ITERATION_LENGTH CONSTANT NUMBER := 4000;
+        v_last_sppcode VARCHAR2 (7)  := '';
         
         SPPSYN CONSTANT VARCHAR2(6) := 'SPPSYN';
         SPPCODE CONSTANT VARCHAR2(7) := 'SPPCODE';
         SPPNAME CONSTANT VARCHAR2(7) := 'SPPNAME';
         ITIS CONSTANT VARCHAR2(4) := 'ITIS';
         X_RPT_TYPE CONSTANT VARCHAR2(10) := 'X_RPT_TYPE';
-        
+
         species_object  JSON_OBJECT_T := json_object_t();
         species_array  JSON_ARRAY_T := json_array_t();
         exclude_array  JSON_ARRAY_T := json_array_t();
         empty_array  JSON_ARRAY_T := json_array_t();
   begin
     DBMS_OUTPUT.ENABLE(1000000);
-    
+
     for s_data in (with species_data as (SELECT spp.sppcode sppsyn,
         spp.sppcode,
         spp.sppname,
@@ -1205,55 +1179,51 @@ procedure permit_json (v_input IN CLOB, v_result OUT clob )
         row_number() OVER (PARTITION BY nvl(FMP_MGMT.GET_SPECIES_ITIS_FNC('SPPCODE', spp.sppcode), 0) order by spp.sppcode)
         ELSE ''
         END itis,
-        spp.required_uom
+        spp.required_uom,
+        ve.exclude_rpt_type
         FROM vtr.vlspptbl spp
+        LEFT OUTER JOIN vtr.vlspecies_exclude ve
+        ON ve.sppcode = spp.sppcode
         WHERE spp.public_access = 'Y'
         AND (FMP_MGMT.GET_SPECIES_ITIS_FNC('SPPCODE', spp.sppcode) is not null OR spp.sppcode = 'NC')
         ORDER BY spp.sppcode ASC)
-    select s.sppsyn, s.sppcode, s.sppname, s.itis
+    select s.sppsyn, s.sppcode, s.sppname, s.itis, s.exclude_rpt_type
     from species_data s) loop
     
-        species_object.put(SPPSYN, s_data.sppsyn);
-        species_object.put(SPPCODE, s_data.sppcode);
-        species_object.put(SPPNAME, s_data.sppname);
-        species_object.put(ITIS, s_data.itis);
-        
-        SELECT count(*) INTO x_exist FROM vtr.vlspecies_exclude
-        WHERE sppcode = s_data.sppcode;
-        
-        IF x_exist > 0 THEN
-            for x_data in (with exclude_data as (select distinct exclude_rpt_type
-                FROM vtr.vlspecies_exclude
-                WHERE sppcode = s_data.sppcode)
-            select x.exclude_rpt_type
-            from exclude_data x) loop
-            
-                exclude_array.append(x_data.exclude_rpt_type);
-            end loop;
-            species_object.put(X_RPT_TYPE, exclude_array);
-            exclude_array := json_array_t();    -- resetting the array
-        END IF;
-        
-         IF ( isi_CurrentDebugLevel >= VERBOSE_DEBUG_LEVEL ) THEN
-            DBMS_OUTPUT.PUT_LINE('Species code:' || species_object.to_clob());
-            DBMS_OUTPUT.PUT_LINE(':'||s_data.sppcode||':');
-         END IF;
-    
-        species_array.append(species_object);
-        --Start cleaning JSON object
-        species_object.remove(SPPSYN);
-        species_object.remove(SPPCODE);
-        species_object.remove(SPPNAME);
-        species_object.remove(ITIS);
-        IF x_exist > 0 THEN
+        IF (s_data.sppsyn = v_last_sppcode) THEN
+            IF (s_data.exclude_rpt_type IS NOT NULL) THEN
+                exclude_array.append(s_data.exclude_rpt_type);
+                species_object.remove(X_RPT_TYPE);
+                species_object.put(X_RPT_TYPE, exclude_array);
+            END IF;
+        ELSE
+            species_array.append(species_object);
+                    --Start cleaning JSON object
+            species_object.remove(SPPSYN);
+            species_object.remove(SPPCODE);
+            species_object.remove(SPPNAME);
+            species_object.remove(ITIS);
             species_object.remove(X_RPT_TYPE);
+            exclude_array := json_array_t();    -- resetting the excluded types array
+            
+            species_object.put(SPPSYN, s_data.sppsyn);
+            species_object.put(SPPCODE, s_data.sppcode);
+            species_object.put(SPPNAME, s_data.sppname);
+            species_object.put(ITIS, s_data.itis);
+       
+            IF (s_data.exclude_rpt_type IS NOT NULL) THEN
+                exclude_array.append(s_data.exclude_rpt_type);
+                species_object.put(X_RPT_TYPE, exclude_array);
+            END IF;
         END IF;
+        
+        v_last_sppcode := s_data.sppsyn;
     end loop;
-    
-     IF ( isi_CurrentDebugLevel >= ERROR_DEBUG_LEVEL ) THEN
+
+     --IF ( isi_CurrentDebugLevel >= ERROR_DEBUG_LEVEL ) THEN
         DBMS_OUTPUT.PUT_LINE('Species JSON:' || species_array.to_clob());
-     END IF;
-    
+     --END IF;
+
     v_result := species_array.to_clob();
 
   --fso_admin.log_event (vbatchprocess, vmodulename, vprocedurename, ifsoseq, 'SUCCESSFUL', 'Successfully finished procedure.' ,vtablename,NULL,NULL,NULL, ilogid);
